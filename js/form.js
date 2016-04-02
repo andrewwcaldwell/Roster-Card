229aab4a322f8888e18262992b7d87fe53b0b436
DeemsW <deems.code@gmail.com>
2016-04-02 09:41:36 -0400

    'Form' dots mostly connected. Inputting info into inputs i  form.html and pressing 'add player' button now updates roster in firebase. Small issue with numbers (age and jersey number) being taken as strings and therefore recorded and reproduced in quotes but we can figure that out when we're cleaning it up. Should just be a parseInt thing.

--- js/form.js
+++ js/form.js
@@ -21,22 +21,31 @@ window.addEventListener('load', function () {
         //console.log('push it');
         var pName = document.getElementById('playerName').value;
         var pAge = document.getElementById('playerAge').value;
+//        var pAgeString = document.getElementById('playerAge').value;
+//        var pAge = parseInt(pAgeString);
         var pNumber = document.getElementById('playerNumber').value;
+//        var pNumberString = document.getElementById('playerNumber').value;
+//        var pNumber = parseInt(pNumberString);
         var pPosition = document.getElementById('playerPosition').value;
         var newPlayer = new Player(pName, pAge, pNumber, pPosition);
         console.log(newPlayer);
-        
+        var fbPlayer = new Firebase('https://team-roster.firebaseio.com/players/' + newPlayer.id);
+        fbPlayer.set(newPlayer, function () {
+            console.log('New dude added');
+        });
     });
-
-    var currentPlayers = new Firebase('https://team-roster.firebaseio.com/players/' + players[i].id);
+    
+    
+    
+//    var currentPlayers = new Firebase('https://team-roster.firebaseio.com/players/' + players[i].id);
 
 /*
 // THIS IS THE CODE THAT LOADED THE INITIAL ROSTER INTO FIREBASE
 // DO NOT RUN/UNCOMMENT THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     for (var i = 0; i < players.length; i++) {
-        var newb = new Player(players[i].name, players[i].age, players[i].number, players[i].position);
-        var friday = new Firebase('https://team-roster.firebaseio.com/players/' + newb.id);
-        friday.set(newb);
+        var newGuy = new Player(players[i].name, players[i].age, players[i].number, players[i].position);
+        var friday = new Firebase('https://team-roster.firebaseio.com/players/' + newGuy.id);
+        friday.set(newGuy);
     }
 */    
 });
